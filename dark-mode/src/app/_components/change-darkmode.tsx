59cc0fd954cec61b47bc765ba7e0786327338582
jordan <jordanelcracken@gmail.com>
2024-05-01 11:05:33 -0500

    Feat: mounted first system behaviour

--- dark-mode/src/app/_components/change-darkmode.tsx
+++ dark-mode/src/app/_components/change-darkmode.tsx
@@ -1,6 +1,19 @@
-import React from 'react'
+"use client"
+import { useTheme } from 'next-themes'
+import React, { useEffect, useState } from 'react'
 
 const SwichDarkLighMode = () => {
+  const [mounted, setMounted] = useState(false)
+  const { theme, setTheme } = useTheme()
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return (
+    <div className=''>Detecting system</div>
+  )
+
   return (
     <div className='flex flex-col gap-2'>
       <button className='px-2 rounded-sm bg-slate-300 dark:text-black transition-all duration-150 hover:scale-105'>Ligh Mode</button>
