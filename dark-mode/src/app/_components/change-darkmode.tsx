06208069d5a54eda5b3f51daf085f33c0c1e5fc4
jordan <jordanelcracken@gmail.com>
2024-05-01 11:07:50 -0500

    feat: working with setTheem light to dark and so on

--- dark-mode/src/app/_components/change-darkmode.tsx
+++ dark-mode/src/app/_components/change-darkmode.tsx
@@ -16,8 +16,9 @@ const SwichDarkLighMode = () => {
 
   return (
     <div className='flex flex-col gap-2'>
-      <button className='px-2 rounded-sm bg-slate-300 dark:text-black transition-all duration-150 hover:scale-105'>Ligh Mode</button>
-      <button className='px-2 rounded-sm bg-slate-300 dark:text-black transition-all duration-150 hover:scale-105'>Dark Mode</button>
+      <p>System say: {theme}</p>
+      <button className='px-2 rounded-sm bg-slate-300 dark:text-black transition-all duration-150 hover:scale-105' onClick={() => setTheme("light")}>Ligh Mode</button>
+      <button className='px-2 rounded-sm bg-slate-300 dark:text-black transition-all duration-150 hover:scale-105' onClick={() => setTheme("dark")}>Dark Mode</button>
     </div>
   )
 }
