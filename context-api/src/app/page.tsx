84d1d1be544701a7364e850fcc3c4d3f5f9040da
jordan <jordanelcracken@gmail.com>
2024-07-07 22:14:13 -0500

    feat: initial commit

---
+++ context-api/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="h-screen  bg-[#282c34] flex justify-center items-center">
+      <p> Hello World  - <span className="text-sky-700 tracking-tighter font-bold">Context-Api</span></p>
+    </main>
+  );
+}
