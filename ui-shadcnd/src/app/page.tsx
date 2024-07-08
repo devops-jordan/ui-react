3d52b4f048f886b553cf74f096d37f7c645214e3
jordan <jordanelcracken@gmail.com>
2024-07-07 22:06:06 -0500

    feat: add reusable button component like shadcdn

---
+++ ui-shadcnd/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/Button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="h-screen bg-[#292d35]">
+      <Button variant="secondary" className="bg-red-400">Click me</Button>
+    </main>
+  );
+}
