9275ee8f0d9415411f35dcd395b577259227cef3
jordan <jordanelcracken@gmail.com>
2024-11-14 15:23:20 -0500

    feat: add installs and routeoutlet to nested dinamic route with the root layout

--- redux/custom-redux/src/main.tsx
+++ redux/custom-redux/src/main.tsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import RootLayout from './routes/root'
+import ErrorPage from './error-page'
+import Contact from './routes/contact'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children:[
+      {
+        path: "contacts/:contactId",
+        element: <Contact />
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
